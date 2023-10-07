e9057c3d0d24fca634718da301fcd8fd6b3aca8c
Neeraj Kumar <130959560+NENEER14@users.noreply.github.com>
2023-10-07 12:17:22 +0530

    Sudoku Code

--- src/main/java/April18Class.java
+++
@@ -1,5 +0,0 @@
-public class April18Class {
-	public static void main(String[] args) {
-		
-	}	
-}
---
+++ src/main/java/SudokuExample.java
@@ -0,0 +1,78 @@
+public class SudokuExample {
+    final static int N = 9;
+
+	static boolean solveSudoku(int grid[][], int row, int col) {
+		if (row == N - 1 && col == N) {
+			return true;
+		}
+		if (col == N) {
+			row++;
+			col = 0;
+		}
+		if (grid[row][col] != 0) {
+			return solveSudoku(grid, row, col + 1);
+		}
+		for (int num = 1; num <= 9; num++) {
+			if (isSafe(grid, row, col, num)) {
+				grid[row][col] = num;
+				if (solveSudoku(grid, row, col + 1)) {
+					return true;
+				}
+			}
+			grid[row][col] = 0;
+		}
+		return false;
+	}
+
+	static boolean isSafe(int grid[][], int row, int col, int num) {
+		for (int i = 0; i < N; i++) {
+			if (grid[row][i] == num) {
+				return false;
+			}
+		}
+		for (int i = 0; i < N; i++) {
+			if (grid[i][col] == num) {
+				return false;
+			}
+		}
+		int startRow = row - row % 3;
+		int startCol = col - col % 3;
+
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				if (grid[startRow + i][startCol + j] == num) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	static void printSudoku(int grid[][]) {
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				System.out.print(grid[i][j] + " ");
+			}
+			System.out.println("");
+		}
+	}
+
+	public static void main(String[] args) {
+
+		int grid[][] = { 	{ 5, 3, 0, 0, 7, 0, 0, 0, 0 },
+							{ 6, 0, 0, 1, 9, 5, 0, 0, 0 },
+							{ 0, 9, 8, 0, 0, 0, 0, 6, 0 },
+							{ 8, 0, 0, 0, 6, 0, 0, 0, 3 },
+							{ 4, 0, 0, 8, 0, 3, 0, 0, 1 },
+							{ 7, 0, 0, 0, 2, 0, 0, 0, 6 },
+							{ 0, 6, 0, 0, 0, 0, 2, 8, 0 },
+							{ 0, 0, 0, 4, 1, 9, 0, 0, 5 },
+							{ 0, 0, 0, 0, 8, 0, 0, 7, 9 } };
+		if (solveSudoku(grid, 0, 0)) {
+			printSudoku(grid);
+		} else {
+			System.out.println("solution does not exist");
+
+		}
+	}
+}
